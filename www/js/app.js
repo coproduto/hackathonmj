3ebde7daccb288f228a0ca53b8db12bee45dc40a
Pedro Castilho <pedro.castilho@rwth-aachen.de>
2016-04-02 03:54:23 -0300

    Added geolocation

--- www/js/app.js
+++ www/js/app.js
@@ -3,9 +3,9 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+var app = angular.module('hackathon', ['ionic','ngCordova'])
 
-.run(function($ionicPlatform) {
+app.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     if(window.cordova && window.cordova.plugins.Keyboard) {
       // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
@@ -22,3 +22,44 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+
+app.controller('MapController', function($scope, $cordovaGeolocation, $ionicLoading, $ionicPlatform) {
+    
+    $ionicPlatform.ready(function() {
+
+	$ionicLoading.show({
+	    template: '<ion-spinner icon="bubbles"></ion-spinner><br/>Acquiring location...'
+	});
+
+
+	var posOptions = {
+	    enableHighAccuracy: true,
+	    timeout: 20000,
+	    maximumAge: 0
+	};
+
+	$cordovaGeolocation.getCurrentPosition(posOptions).then(function(position) {
+	    var lat = position.coords.latitude;
+	    var lng = position.coords.longitude;
+
+	    var myLatLng = new google.maps.LatLng(lat,lng);
+
+	    var mapOptions = {
+		center: myLatLng,
+		zoom: 16,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	    };
+
+	    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+	    $scope.map = map;
+	    $ionicLoading.hide();
+
+	}, function(err) {
+	    $ionicLoading.hide();
+	    console.log(err);
+	});
+    });
+});
+    
+		  
